d76df7fc295219a8205e20dd5dee19775eed71b8
arkneeland <67293380+arkneeland@users.noreply.github.com>
2020-07-07 11:16:09 -0700

    Initial commit

---
+++ src/components/SectionLocations/SectionLocations.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from '../../util/reactIntl';
+import classNames from 'classnames';
+import { lazyLoadWithDimensions } from '../../util/contextHelpers';
+
+import { NamedLink } from '../../components';
+
+import css from './SectionLocations.css';
+
+import arlingtonImage from './images/location_arlington.jpg';
+import marysvilleImage from './images/location_marysville.jpg';
+import everettImage from './images/location_everett.jpg';
+
+class LocationImage extends Component {
+  render() {
+    const { alt, ...rest } = this.props;
+    return <img alt={alt} {...rest} />;
+  }
+}
+const LazyImage = lazyLoadWithDimensions(LocationImage);
+
+const locationLink = (name, image, searchQuery) => {
+  const nameText = <span className={css.locationName}>{name}</span>;
+  return (
+    <NamedLink name="SearchPage" to={{ search: searchQuery }} className={css.location}>
+      <div className={css.imageWrapper}>
+        <div className={css.aspectWrapper}>
+          <LazyImage src={image} alt={name} className={css.locationImage} />
+        </div>
+      </div>
+      <div className={css.linkText}>
+        <FormattedMessage
+          id="SectionLocations.listingsInLocation"
+          values={{ location: nameText }}
+        />
+      </div>
+    </NamedLink>
+  );
+};
+
+const SectionLocations = props => {
+  const { rootClassName, className } = props;
+
+  const classes = classNames(rootClassName || css.root, className);
+
+  return (
+    <div className={classes}>
+      <div className={css.title}>
+        <FormattedMessage id="SectionLocations.title" />
+      </div>
+      <div className={css.locations}>
+        {locationLink(
+          'Arlington',
+          arlingtonImage,
+          '?address=98223'
+        )}
+        {locationLink(
+          'Marysville',
+          marysvilleImage,
+          '?address=98270'
+        )}
+        {locationLink(
+          'Everett',
+          everettImage,
+          '?address=98201'
+        )}
+      </div>
+    </div>
+  );
+};
+
+SectionLocations.defaultProps = { rootClassName: null, className: null };
+
+const { string } = PropTypes;
+
+SectionLocations.propTypes = {
+  rootClassName: string,
+  className: string,
+};
+
+export default SectionLocations;
